35d2a73356c6593d7609a90fe25500a5a93ade96
Richardbmk <rdobmk@gmail.com>
2026-08-27 14:41:40 +0200

    Deploy web subnet with nsg and rules

--- 00-Projec02-LinuxVM/locals.tf
+++ 00-Projec02-LinuxVM/locals.tf
@@ -5,6 +5,13 @@ locals {
   resource_group_prefix = "${var.business_division}-${var.environment}"
   name                  = "${local.owners}-${local.environment}"
 
+  ## Locals Block for Security Rules
+  web_inbound_ports_map = {
+    "100" : "80", # If the key starts with a number, you must use the colon syntax ":" instead of "="
+    "110" : "443",
+    "120" : "22"
+  }
+
   common_tags = {
     owners      = local.owners
     environment = local.environment
