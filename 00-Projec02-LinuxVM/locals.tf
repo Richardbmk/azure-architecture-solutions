74be2ea134d57118a48f18ae6418bfacba5756fe
Richardbmk <rdobmk@gmail.com>
2026-08-28 15:09:20 +0200

    Deploy webserver VM

--- 00-Projec02-LinuxVM/locals.tf
+++ 00-Projec02-LinuxVM/locals.tf
@@ -22,25 +22,25 @@ locals {
   }
 
   app_inbound_ports_map = {
-    "100" : "80", # If the key starts with a number, you must use the colon syntax ":" instead of "="
+    "100" : "80",
     "110" : "443",
     "120" : "8080",
     "130" : "22"
   }
 
   db_inbound_ports_map = {
-    "100" : "3306", # If the key starts with a number, you must use the colon syntax ":" instead of "="
+    "100" : "3306",
     "110" : "1433",
     "120" : "5432"
   }
 
   bastion_inbound_ports_map = {
-    "100" : "22", # If the key starts with a number, you must use the colon syntax ":" instead of "="
+    "100" : "22",
     "110" : "3389"
   }
 
   web_vmnic_inbound_ports_map = {
-    "100" : "80", # If the key starts with a number, you must use the colon syntax ":" instead of "="
+    "100" : "80",
     "110" : "443",
     "120" : "22"
   }
