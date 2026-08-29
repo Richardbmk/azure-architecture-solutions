52daa5b3e07bbe933861a5da138263d7ad9b8baf
Richardbmk <rdobmk@gmail.com>
2026-08-29 09:56:49 +0200

    Delete new project

--- 01-Project02-Standard-ALB/providers.tf
+++
@@ -1,4 +0,0 @@
-# Provider Block
-provider "azurerm" {
-  features {}
-}
--- 01-Project02-Standard-ALB/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "existing_resource_group_name" {
-  description = "Existing Resource Group Name"
-  type        = string
-  default     = ""
-}
